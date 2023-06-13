7bfac6ef91581b50e33b60b642631f215a2407ea
racalixta <ra.calixta@gmail.com>
2023-06-13 15:47:49 -0300

    conteudo da aula 15

--- lib/page/auth_page.dart
+++ lib/page/auth_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gymapp/_common/my_colors.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool wantEntry = true;
 
   @override
   Widget build(BuildContext context) {
@@ -22,24 +29,83 @@ class AuthPage extends StatelessWidget {
               ),
             ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset(
-                "assets/dumbbell.png",
-                height: 128,
-              ),
-              const Text(
-                "GymApp",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 48,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Image.asset(
+                        "assets/dumbbell.png",
+                        height: 128,
+                      ),
+                      const Text(
+                        "GymApp",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 32,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text("E-mail"),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text("Senha"),
+                        ),
+                        obscureText: true,
+                      ),
+                      Visibility(
+                        visible: !wantEntry,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                label: Text("Confirme senha"),
+                              ),
+                              obscureText: true,
+                            ),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                label: Text("Nome"),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text((wantEntry) ? "Entrar" : "Cadastrar"),
+                      ),
+                      // const Divider(),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            wantEntry = !wantEntry;
+                          });
+                        },
+                        child: Text((wantEntry)
+                            ? "Ainda não tem uma conta? Cadastre-se aqui!"
+                            : "Já possui uma conta? Clique aqui para entrar!"),
+                      ),
+                    ],
+                  ),
                 ),
-              )
-            ],
+              ),
+            ),
           ),
         ],
       ),
